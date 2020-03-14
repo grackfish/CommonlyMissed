ee2eeb5b7cee526813b4eebd1c79ea13e960d769
Grace Liu <graceshiliu@gmail.com>
2020-03-14 13:23:56 -0700

    Finished

---
+++ ThreeSixMain.java
@@ -0,0 +1,29 @@
+public class ThreeSixMain {
+    public static void main() {
+        System.out.println("Original output:");
+        ThreeSix.main();
+        System.out.println();
+        
+        System.out.println("Choice A:");
+        ThreeSixA.main();
+        System.out.println();
+        
+        System.out.println("Choice B:");
+        ThreeSixB.main();
+        System.out.println();
+        
+        System.out.println("Choice C:");
+        ThreeSixC.main();
+        System.out.println();
+        
+        System.out.println("Choice D:");
+        ThreeSixD.main();
+        System.out.println();
+        
+        System.out.println("Choice E:");
+        ThreeSixE.main();
+        System.out.println();
+        
+        
+    }
+}
